50599a01a9e17946dd265af890097b8392b1a090
Puja <kripuja1999@gmail.com>
2023-06-21 08:53:27 +0530

    New Files

---
+++ app/src/main/java/com/puja/lifecycle/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.puja.lifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.scrollview_example)
+
+        title = "The Avengers"
+    }
+}
